d9ddf977c75a02e6213bde3a405d7a11786a2bb0
Lucas Martins <lucasfm801@gmail.com>
2025-06-27 13:32:36 -0300

    contact

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,6 +4,8 @@ import styles from "./ContactStyles.module.css";
 const Contact = () => {
   return <section id="contact" className={styles.container}>
     <h1>Contact</h1>
+    <p> Email - lucasfm801@gmail.com</p>
+    <p> Phone - 55 (48)99110-9719</p>
   </section>;
 };
 
